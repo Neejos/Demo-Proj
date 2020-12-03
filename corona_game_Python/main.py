0a3600be030ebbbb06173f2739f39b93b21cda98
dhawan-mani <65930020+dhawan-mani@users.noreply.github.com>
2020-12-03 13:10:09 -0800

    Organized flask app

---
+++ corona_game_Python/main.py
@@ -0,0 +1,21 @@
+
+from flask import Flask, render_template,redirect,request
+# from flex import app,event,ui
+# app.init()
+# Create an instance of Flask app
+app= Flask(__name__,static_url_path='/static')
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+# @app.route("/home")
+# def Session():
+#     import game
+#     return game_start()
+   
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
