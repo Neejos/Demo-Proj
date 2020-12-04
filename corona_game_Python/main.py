1ce3d76f70962d99b2777001b2030b6c97942e86
dhawan-mani <65930020+dhawan-mani@users.noreply.github.com>
2020-12-03 19:20:00 -0800

    Database setup

--- corona_game_Python/main.py
+++ corona_game_Python/main.py
@@ -1,12 +1,75 @@
-
-from flask import Flask, render_template,redirect,request
+from flask import Flask, render_template,redirect,request,flash
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 # from flex import app,event,ui
 # app.init()
 # Create an instance of Flask app
 app= Flask(__name__,static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/Corona_Hit"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-@app.route("/")
+class FormDetails(db.Model):
+    __tablename__ = 'corona_user_data'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    phone = db.Column(db.Integer())
+    message = db.column(db.String())
+    BG = db.column(db.String())
+    CS = db.column(db.String())
+    Date = db.column(db.Date())
+
+
+
+    # def __repr__(self):
+    #     return f"<Name {self.name}>"
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+    email = TextField('Email:', validators=[validators.required()])
+    CS = TextField('Corona Survivor(Yes/No):', validators=[validators.required()])
+    BG = TextField('Blood Group:', validators=[validators.required()])
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    form = ReusableForm(request.form)
+
+    print(form.errors)
+    if request.method == 'POST':
+        name=request.form['Name']
+        email = request.form['Email']
+        phone = request.form['Phone']
+        date = request.form['Date']
+        BG = request.form['Blood Group']
+        CS = request.form['Corona Survivor(Yes/No)']
+        message = request.form['form_message']
+        print(name,email,phone,date,message,BG,CS,message)
+
+    def __init__(self, name, email,phone,message,BG,CS,Date):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.message = message
+        self.BG = BG
+        self.CS = CS
+        self.Date = Date
+        self.id = id
+    def __repr__(self):
+        return f"<Car {self.name}>"
+
+    
+
+    if form.validate():
+        # Save the comment here.
+        flash('Hello ' + name)
+    else:
+        flash('All the form fields are required. ')
+    
+    # return render_template('hello.html', form=form)
     return render_template('index.html')
 
 @app.route("/about")
@@ -21,7 +84,10 @@ def insight():
 def appointment():
     return render_template('appointment.html')
 
-
+@app.route("/submit.html")
+def submit():
+    
+    return render_template('submit.html')
 
 # @app.route("/home")
 # def Session():
---
+++ corona_game_Python/templates/sendmail.php
@@ -0,0 +1,38 @@
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $Phone = $BloodGroup = $form_message"";
+$name = $email = $form_message = $Phone = $BloodGroup = "";
+
+echo("hello");
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//   if (empty($_POST["name"])) {
+//     $nameErr = "Name is required";
+//   } else {
+//     $name = test_input($_POST["name"]);
+//   }
+
+//   if (empty($_POST["email"])) {
+//     $emailErr = "Email is required";
+//   } else {
+//     $email = test_input($_POST["email"]);
+//   }
+
+//   if (empty($_POST["Phone"])) {
+//     $website = "";
+//   } else {
+//     $website = test_input($_POST["Phone"]);
+//   }
+
+//   if (empty($_POST["form_message"])) {
+//     $comment = "";
+//   } else {
+//     $comment = test_input($_POST["comment"]);
+//   }
+
+//   if (empty($_POST["gender"])) {
+//     $genderErr = "Gender is required";
+//   } else {
+//     $gender = test_input($_POST["gender"]);
+//   }
+// }
+// ?>
