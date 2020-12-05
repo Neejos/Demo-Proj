7b99d4815cfb360d1e8b0b4c9ebae6aba0dcca1c
dhawan-mani <65930020+dhawan-mani@users.noreply.github.com>
2020-12-04 18:29:12 -0800

    Updated db methods

--- corona_game_Python/main.py
+++ corona_game_Python/main.py
@@ -104,6 +104,41 @@ def post_user():
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('home'))
+
+@app.route("/submit.html,methods=['GET','POST']")
+def sign_user():
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('Name')  # access the data inside 
+        email = request.form.get('Email')
+        # query = FormDetails.filter(FormDetails.name==(username),FormDetails.email==(POST_email))
+        # results = FormDetails.query.all()
+        users = FormDetails.query.all()
+        results = [
+            {
+                "name": user.name,
+                "email": user.email,
+            } for user in users]
+        for result in results:
+            if username == result['name'] and email == result['email']:
+                message = "Correct username and password"
+                return render_template('submit.html',message=message)
+            else:
+                message = "Wrong username or password"
+                return render_template('index.html',message=message)
+
+
+    # return redirect(url_for('home'))
+    
+    
+
+
+@app.route("/appointment")
+def appointment():
+  
+    # return render_template('login.html', message=message
+    return render_template('appointment.html')
+    
    
     
 @app.route("/about")
@@ -114,9 +149,6 @@ def about():
 def insight():
     return render_template('insight.html')
 
-@app.route("/appointment")
-def appointment():
-    return render_template('appointment.html')
 
 @app.route("/submit.html")
 def submit():
